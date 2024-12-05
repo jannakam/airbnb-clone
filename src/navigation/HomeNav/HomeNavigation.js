aed19c634ff5f6c8f207c8fd35ef438faa543cae
jannakam <jannakalmuqaisib@gmail.com>
2024-12-05 08:30:44 +0300

    nav bar

--- src/navigation/HomeNav/HomeNavigation.js
+++ src/navigation/HomeNav/HomeNavigation.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Explore from '../../screens/Explore'
+import Messages from '../../screens/Messages'
+import Profile from '../../screens/Profile'
+import Wishlists from '../../screens/Wishlists'
+import Trips from '../../screens/Trips'
+
+
 
 const HomeNavigation = () => {
+  const Stack = createNativeStackNavigator()
   return (
-    <View>
-      <Text>HomeNavigation</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Explore" component={Explore} />
+      <Stack.Screen name="Messages" component={Messages} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Trips" component={Settings} />
+      <Stack.Screen name="Wishlists" component={Wishlists} />
+
+    </Stack.Navigator>
   )
 }
 
