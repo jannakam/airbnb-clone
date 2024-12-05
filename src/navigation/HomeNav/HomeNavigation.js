b4189401261b3ddfaf38ec170fa20e22a18ed573
jannakam <jannakalmuqaisib@gmail.com>
2024-12-05 09:07:52 +0300

    fixed icons

--- src/navigation/HomeNav/HomeNavigation.js
+++ src/navigation/HomeNav/HomeNavigation.js
@@ -7,18 +7,18 @@ import Wishlists from '../../screens/Wishlists'
 import Trips from '../../screens/Trips'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
-
+import Icon5 from 'react-native-vector-icons/FontAwesome5'
 
 
 const HomeNavigation = () => {
   const Tab = createBottomTabNavigator()
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Explore" component={Explore} options={{tabBarIcon: ({color, size}) => <Icon name="home" color={color} size={size} />}}/>
-      <Tab.Screen name="Messages" component={Messages} options={{tabBarIcon: ({color, size}) => <Icon name="envelope-o" color={color} size={size} />}}/>
-      <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({color, size}) => <Icon name="user" color={color} size={size} />}}/>
-      <Tab.Screen name="Trips" component={Trips} options={{tabBarIcon: ({color, size}) => <Icon name="trip" color={color} size={size} />}}/>
+      <Tab.Screen name="Explore" component={Explore} options={{tabBarIcon: ({color, size}) => <Icon name="search" color={color} size={size} />}}/>
       <Tab.Screen name="Wishlists" component={Wishlists} options={{tabBarIcon: ({color, size}) => <Icon name="heart-o" color={color} size={size} />}}/>
+      <Tab.Screen name="Trips" component={Trips} options={{tabBarIcon: ({color, size}) => <Icon5 name="airbnb" color={color} size={size} />}}/>
+      <Tab.Screen name="Messages" component={Messages} options={{tabBarIcon: ({color, size}) => <Icon name="envelope-o" color={color} size={size} />}}/>
+      <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({color, size}) => <Icon name="user-o" color={color} size={size} />}}/>
     </Tab.Navigator>
   )
 }
