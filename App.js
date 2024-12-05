8cdba7e1817eb90b8cd7e43ef47bfd0a8e57f447
jannakam <jannakalmuqaisib@gmail.com>
2024-12-05 08:48:43 +0300

    asdaw

--- App.js
+++ App.js
@@ -10,21 +10,16 @@ import { StatusBar } from "expo-status-bar";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from "react-native";
+import HomeNavigation from "./src/navigation/HomeNav/HomeNavigation";
+import { NavigationContainer } from "@react-navigation/native";
+
+
 export default function App() {
   return (
     <GluestackUIProvider mode="light">
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Button
-          action={"primary"}
-          variant={"outline"}
-          size={"lg"}
-          isDisabled={false}
-        >
-          <ButtonText>Hello World</ButtonText>
-        </Button>
-      </View>
+      <NavigationContainer>
+        <HomeNavigation />
+      </NavigationContainer>
     </GluestackUIProvider>
   );
 }
--- src/navigation/HomeNav/HomeNavigation.js
+++ src/navigation/HomeNav/HomeNavigation.js
@@ -1,25 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Explore from '../../screens/Explore'
 import Messages from '../../screens/Messages'
 import Profile from '../../screens/Profile'
 import Wishlists from '../../screens/Wishlists'
 import Trips from '../../screens/Trips'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 
 const HomeNavigation = () => {
-  const Stack = createNativeStackNavigator()
+  const Tab = createBottomTabNavigator()
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Explore" component={Explore} />
-      <Stack.Screen name="Messages" component={Messages} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Trips" component={Settings} />
-      <Stack.Screen name="Wishlists" component={Wishlists} />
-
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Explore" component={Explore} options={{tabBarIcon: ({color, size}) => <Icon name="home" color={color} size={size} />}}/>
+      <Tab.Screen name="Messages" component={Messages} options={{tabBarIcon: ({color, size}) => <Icon name="envelope-o" color={color} size={size} />}}/>
+      <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({color, size}) => <Icon name="user" color={color} size={size} />}}/>
+      <Tab.Screen name="Trips" component={Trips} options={{tabBarIcon: ({color, size}) => <Icon name="trip" color={color} size={size} />}}/>
+      <Tab.Screen name="Wishlists" component={Wishlists} options={{tabBarIcon: ({color, size}) => <Icon name="heart-o" color={color} size={size} />}}/>
+    </Tab.Navigator>
   )
 }
 
